dbf388f7b9f4a90c01f17b0db960dfec8e33e164
Andrey <dewshick@gmail.com>
2019-12-11 01:59:18 +0300

    implement basic sample_dist and score_dist

--- src/Eval.ml
+++ src/Eval.ml
@@ -34,11 +34,15 @@ module Eval (R : R) =
 (* Likelihood Weighted Importance Sampler *)
 module LWIS =
   struct
-    
-    let sample_dist = invalid_arg ""
+    let sample_dist = function 
+      | "flip", [Num p; Num t; Num f] -> if Random.float 1.0 < p then t else f
+      | "uniform", [Num beg; Num len] -> beg +. Random.float len
 
-    let score_dist = invalid_arg ""
-  
+    let score_dist = function
+      | "flip", [Num p; Num t; Num f], v -> if (v -. t < 0.0001) then t else 
+                                            if (v -. f < 0.0001) then f else 0.0
+      | "uniform", _, _ -> 0.0 (* TODO: support interval restrictions *)
+
     let reduce = function
       | If  (Bool true, e, _), w -> e, w
       | If  (_,         _, e), w -> e, w
@@ -50,10 +54,10 @@ module LWIS =
   
       | App [Sample; App (Dist d :: args)], w ->
          (* d = flip | normal | poisson *)
-         sample_dist d args, w
+         Num (sample_dist (d, args)), w
         
       | App ([Observe; App (Dist d :: args); c]), w ->
-         c, w * score_dist d args c 
+         c, w * score_dist (d, args, c) 
   end
 
 (* General Weighted Importance Sampler *) 
